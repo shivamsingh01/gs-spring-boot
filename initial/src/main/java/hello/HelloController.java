8cfb98073dcf32efbf44f6fb940fa4ee3710f068
shivamsingh01 <shivam10.mzn@gmail.com>
2019-03-10 15:00:05 +0530

    Added support for port number.

--- initial/src/main/java/hello/HelloController.java
+++ initial/src/main/java/hello/HelloController.java
@@ -6,9 +6,14 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RestController
 public class HelloController {
     
-    @RequestMapping("/")
+    @RequestMapping("/hello")
     public String index() {
         return "Greetings from Spring Boot!";
+    }
+
+    @RequestMapping("/helloworld")
+    public String index1() {
+        return "hello world!";
     }
     
 }
